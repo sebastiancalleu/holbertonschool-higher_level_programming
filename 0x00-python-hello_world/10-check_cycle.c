112a3cfa0770315ff72a633ec8e79446455dd819
sebastian <sebastian.calleu@gmail.com>
2020-12-07 17:12:00 -0500

    factor(docs): adding the files for first time

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * check_cycle - this function checks if a listed link has a loop.
+ * @list: the list.
+ * Return: 1 if there is a loop, 0 if not.
+ */
 int check_cycle(listint_t *list)
 {
-	if (list)
-		printf("holi");
-	else
-		printf("wtf");
-	return (1);
+listint_t *listft, *listsl;
+
+listft = list;
+listsl = list;
+while (listft != NULL)
+{
+	listsl = listsl->next;
+	listft = listft->next;
+	listft = listft->next;
+	if (listft == listsl)
+		return (1);
+}
+return (0);
 }
