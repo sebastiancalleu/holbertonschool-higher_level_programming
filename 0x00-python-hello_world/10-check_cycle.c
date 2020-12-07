6c67c4e43a4bee9ab9ee618c0c1c469701425a8a
sebastian <sebastian.calleu@gmail.com>
2020-12-07 13:45:43 -0500

    factor(all): adding al the files for first time

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+int check_cycle(listint_t *list)
+{
+	if (list)
+		printf("holi");
+	else
+		printf("wtf");
+	return (1);
+}
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+tristr = 3 * str
+print ('{}'.format(tristr))
+print ('{}'.format(str[:9]))
