c82e062a66fffdb0a8b41871f1969549a5426a53
sebastian <sebastian.calleu@gmail.com>
2020-12-08 14:43:27 -0500

    factor(9): removing variables

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
     if number < 0:
-        i = number % -10
-        i = -i
-    else:
-        i = number % 10
-    print("{:d}".format(i), end='')
-    return(i)
+        number = -number
+    print("{:d}".format(number % 10), end='')
+    return(number % 10)
