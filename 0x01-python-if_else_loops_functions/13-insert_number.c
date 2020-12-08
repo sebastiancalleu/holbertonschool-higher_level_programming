4abceb70676fd809aad684ae2a9b8280be6d2791
sebastian <sebastian.calleu@gmail.com>
2020-12-08 16:13:18 -0500

    factor(13): adding a node in a sorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * insert_node - this function adds a node at a sorted list.
+ * @head: the list.
+ * @number: the data to add.
+ * Return: the new node.
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	int a, flag = 0;
+	listint_t *tmp1, *tmp2;
+	listint_t *new;
+
+	tmp1 = *head;
+	tmp2 = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+	if ((*head)->n >= number)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		tmp1 = tmp1->next;
+		for (a = 0; tmp1 != NULL; a++)
+		{
+			if (tmp1->n >= number)
+			{
+				tmp2->next = new;
+				new->next = tmp1;
+				flag = 1;
+				break;
+			}
+			else
+				tmp1 = tmp1->next;
+				tmp2 = tmp2->next;
+		}
+		if (flag == 0)
+		{
+			tmp2->next = new;
+			new->next = tmp1;
+		}
+	}
+	return (new);
+}
