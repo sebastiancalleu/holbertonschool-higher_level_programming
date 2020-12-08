6234053db14560e3016ce0eced165018d0b09dfc
sebastian <sebastian.calleu@gmail.com>
2020-12-08 13:50:32 -0500

    style(all)fix(6): fixing style, removing when i == j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in range (0, 9):
-	for j in range (0, 10):
-		if i == 8 and j == 9:
-			print("{:d}{:d}".format(i, j))
-		elif i > j or (i == 0 and j == 0):
-			pass
-		else:
-			print("{:d}{:d}, ".format(i, j), end= '')
+for i in range(0, 9):
+    for j in range(0, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        elif (i > j or (i == 0 and j == 0) or i == j):
+            pass
+        else:
+            print("{:d}{:d}, ".format(i, j), end='')
