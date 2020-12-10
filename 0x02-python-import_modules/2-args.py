73e355a0413998f8a2f0fc030aff327e8b4c11d4
sebastian <sebastian.calleu@gmail.com>
2020-12-10 13:26:08 -0500

    refactor(stringformat): implementing the string format in print.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    print("{}".format(len(sys.argv) - 1), end="")
     if (len(sys.argv) == 1):
-        print("{} arguments.".format(len(sys.argv) - 1))
+        print(" arguments.")
     elif (len(sys.argv) == 2):
-        print("{} argument:".format(len(sys.argv) - 1))
-        print("{}: ".format(len(sys.argv) - 1), sys.argv[1])
+        print(" argument:")
+        print("{}: {}".format(len(sys.argv) - 1, sys.argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print(" arguments:")
         for i in range(1, len(sys.argv)):
-            print("{}: ".format(i), sys.argv[i])
+            print("{}: {}".format(i, sys.argv[i]))
