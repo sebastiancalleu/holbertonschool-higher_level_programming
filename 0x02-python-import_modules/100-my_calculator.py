5eb74ad012bfe583c35fb144134a5fff279b7749
sebastian <sebastian.calleu@gmail.com>
2020-12-10 14:26:54 -0500

    refactor(100): removing a check print

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
-    print(len(sys.argv))
+    from calculator_1 import add, sub, mul, div
     if(len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
