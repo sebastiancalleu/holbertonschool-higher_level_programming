89fd849ee063107c12b4f332ec80ed955b203858
sebastian <sebastian.calleu@gmail.com>
2020-12-15 15:44:35 -0500

    factor(9): modifying min number.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return (None)
     else:
-        max = 0
+        max = -1000000000
         for i in range(0, len(my_list)):
             if (my_list[i] > max):
                 max = my_list[i]
