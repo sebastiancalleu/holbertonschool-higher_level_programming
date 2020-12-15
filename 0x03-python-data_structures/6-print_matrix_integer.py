95f2644d8b1e2656208bc73ed12a49fd62316a06
sebastian <sebastian.calleu@gmail.com>
2020-12-15 14:56:58 -0500

    factor(if): deleting edge case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if (len(matrix) == 1):
-        print("")
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             if (j == (len(matrix[i]) - 1)):
