588d6de702734d0ef74407e64e3163bed252803c
sebastian <sebastian.calleu@gmail.com>
2020-12-15 15:13:12 -0500

    style(space): removing space from format string.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             if (j == (len(matrix[i]) - 1)):
-                print("{:d}". format(matrix[i][j]))
+                print("{:d}".format(matrix[i][j]))
             else:
-                print("{:d}". format(matrix[i][j]), end=" ")
+                print("{:d}".format(matrix[i][j]), end=" ")
