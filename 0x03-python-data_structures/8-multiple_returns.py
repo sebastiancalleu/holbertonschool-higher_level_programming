d640a5ae7a4ee4809b1a63c20119961651fad5e9
sebastian <sebastian.calleu@gmail.com>
2020-12-15 15:33:31 -0500

    factor(8): fixing edge case.

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if (len(sentence) == 0):
-        newtuple = (None, sentence[0])
+        newtuple = (len(sentence), None)
     else:
         newtuple = ((len(sentence)), sentence[0])
     return(newtuple[0], newtuple[1])
