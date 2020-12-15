919b9872ad08480308fd4808aa51755a6df99524
sebastian <sebastian.calleu@gmail.com>
2020-12-15 14:02:51 -0500

    style(pep8): fixing code style.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     newstring = ""
-    for i in range (0, len(my_string)):
+    for i in range(0, len(my_string)):
         if (my_string[i] == "C" or my_string[i] == "c"):
             pass
         else:
