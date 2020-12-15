38859c18411d9a5f916f723ef6967ca85ca13a04
sebastian <sebastian.calleu@gmail.com>
2020-12-15 14:04:11 -0500

    factor(pass): removing pass function.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     newstring = ""
     for i in range(0, len(my_string)):
         if (my_string[i] == "C" or my_string[i] == "c"):
-            pass
+            {}
         else:
             newstring = newstring + my_string[i]
     return (newstring)
