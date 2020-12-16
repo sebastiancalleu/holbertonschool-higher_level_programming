8fd90e8d8fcd6943cb134b3c957833facf2e97ac
sebastian <sebastian.calleu@gmail.com>
2020-12-15 23:57:08 -0500

    factor(Python): using the python object.

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,33 @@
+#include "Python.h"
 
+/**
+ * print_python_list_info - print some python info.
+ * @p: pointer to Pyobject.
+ */
 
 void print_python_list_info(PyObject *p)
 {
-	
+	int a, b;
+	PyObject *element;
+
+	b = PyList_Size(p);
+
+	PyListObject *aux = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", b);
+	printf("[*] Allocated = %li\n", aux->allocated);
+	for (a = 0; a < b; a++)
+	{
+		item = PyList_GetItem(p, a);
+		if (PyFloat_Check(element))
+			printf("Element %d: float\n", a);
+		if (PyTuple_Check(element))
+			printf("Element %d: tuple\n", a);
+		if (PyList_Check(element))
+			printf("Element %d: list\n", a);
+		if (PyLong_Check(element))
+			printf("Element %d: int\n", a);
+		if (PyUnicode_Check(element))
+			printf("Element %d: str\n", a);
+	}
 }
