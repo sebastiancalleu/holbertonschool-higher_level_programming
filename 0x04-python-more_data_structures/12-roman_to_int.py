6c203ddc5db3eafad730685546331d41e7f820a6
sebastian <sebastian.calleu@gmail.com>
2020-12-17 15:39:49 -0500

    factor(edges): adding edge cases.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return (0)
     roman_dictionary = {'I': 1, 'V': 5, 'X': 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum = 0
     for i in range(0, len(roman_string)):
