cc0f074f3818d50a9e1ffcf2c415059378f493eb
sebastian <sebastian.calleu@gmail.com>
2020-12-17 15:41:49 -0500

    style(pep8): fixing code style.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,13 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return (0)
-    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, "L": 50,
+                        "C": 100, "D": 500, "M": 1000}
     sum = 0
     for i in range(0, len(roman_string)):
         if (i + 1 < len(roman_string)):
-            if (roman_dictionary[roman_string[i]] >= roman_dictionary[roman_string[i + 1]]):
+            if (roman_dictionary[roman_string[i]] >=
+               roman_dictionary[roman_string[i + 1]]):
                 sum += roman_dictionary[roman_string[i]]
             else:
                 sum -= roman_dictionary[roman_string[i]]
