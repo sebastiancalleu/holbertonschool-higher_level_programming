196c90dfd4ebd40715bce7e3d5411cc902b76aa7
Sebastian <sebastian.calleu@gmail.com>
2021-01-04 15:48:02 -0500

    factor(type): adding type error.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,4 +6,7 @@ def safe_print_integer_err(value):
     except ValueError as er:
         print("Exception:", er, file=sys.stderr)
         return (False)
+    except TypeError as er:
+        print("Exception:", er, file=sys.stderr)
+        return (False)
     return (True)
