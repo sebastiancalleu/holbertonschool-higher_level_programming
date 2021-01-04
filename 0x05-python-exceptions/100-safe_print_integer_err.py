3b73a253416317f9b023498b80ab146f2399260a
Sebastian <sebastian.calleu@gmail.com>
2021-01-04 15:45:46 -0500

    factor(100): adding file for first time.

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as er:
+        print("Exception:", er, file=sys.stderr)
+        return (False)
+    return (True)
