361106297377937afa291809cc93b556d942db0d
sebastian <sebastian.calleu@gmail.com>
2021-01-05 17:47:20 -0500

    factor(2): adding file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class to define a square"""
+
+
+class Square:
+    """
+    square class to define a square
+    attributes:
+            size: private instance attribute (int).
+    """
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
