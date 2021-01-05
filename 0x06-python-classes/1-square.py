e5ff0816d7eea8844a662735d5027b41786f9de2
sebastian <sebastian.calleu@gmail.com>
2021-01-05 17:27:38 -0500

    factor(1): adding files for first time.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class to define a square"""
+
+
+class Square:
+    """
+    square class to define a square
+    attributes:
+            size: private instance attribute
+    """
+    def __init__(self, size):
+        self.__size = size
