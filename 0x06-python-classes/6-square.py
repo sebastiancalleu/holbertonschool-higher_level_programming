2b8e2deb2249a9ecc46a56c2edbf5363ac0423fb
sebastian <sebastian.calleu@gmail.com>
2021-01-05 22:43:12 -0500

    factor(init): modifying types.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,8 +9,8 @@ class Square:
             size: private instance attribute (int).
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
