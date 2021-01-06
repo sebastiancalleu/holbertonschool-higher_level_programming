46889294b7bd25c8ad9a8a37d38e469efb7b8b20
sebastian <sebastian.calleu@gmail.com>
2021-01-05 23:14:32 -0500

    creating file

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    c = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                c += (a ** b) / i
+        except:
+            c = b + a
+            break
+    return c
