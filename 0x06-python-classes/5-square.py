2cb6fc00d7b368b4685bea451f59f9b36d795402
sebastian <sebastian.calleu@gmail.com>
2021-01-05 20:35:28 -0500

    factor(5): adding files for first time.

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""class to define a square"""
+
+
+class Square:
+    """
+    square class to define a square
+    attributes:
+            size: private instance attribute (int).
+    """
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """
+    public instance method that calculates the area of a square
+    """
+    def area(self):
+        return self.__size * self.__size
+
+    """
+    public instance method that print a square with "#" characters
+    """
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
