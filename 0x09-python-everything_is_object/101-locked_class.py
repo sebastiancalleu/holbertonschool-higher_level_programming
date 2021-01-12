f24be2a0cefdd68327ee69e701e93a15e6576d7d
sebastian <sebastian.calleu@gmail.com>
2021-01-12 18:39:56 -0500

    docs(lockedclass): adding documentation.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+class to lock instance attributes
+"""
+
+
 class LockedClass:
+    """
+    LockedClass
+    class with locked
+    instance attributes
+    """
+
     __slots__ = "first_name"
