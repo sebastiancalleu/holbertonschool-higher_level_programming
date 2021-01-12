66a565e58b603b71c8ad4819e2cfcd3b095eb33b
sebastian <sebastian.calleu@gmail.com>
2021-01-12 18:23:07 -0500

    adding files for first time.

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(l1=[]):
+    l1 += ["holberton"]
+    return ", ".join(l1)
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,2 @@
+#Everything is object#
+this project is for learn more about how objects work in python
