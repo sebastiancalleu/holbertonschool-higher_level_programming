19a272bcc31d7192dca4e57f2a312820746e48ea
sebastian <sebastian.calleu@gmail.com>
2021-01-19 18:53:30 -0500

    factor(self): using super()

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle(BaseGeometry):
         width = the width of the rectangle.
         height = the height of the rectangle.
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
