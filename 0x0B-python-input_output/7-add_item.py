efaff8f026ac26a5c9a88140dfc407dfa4ceca78
sebastian <sebastian.calleu@gmail.com>
2021-01-20 19:49:00 -0500

    factor(try): modifyin try

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,20 +7,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == '__main__':
-    list1 = []
-
     try:
         list1 = load_from_json_file("add_item.json")
+        list1 += sys.argv[1:]
+        save_to_json_file(list(list1), "add_item.json")
     except Exception:
-        args = list(sys.argv)
+        save_to_json_file(list(sys.argv[1:]), "add_item.json")
 
-        for i in range(1, len(args)):
-            list1.append(args[i])
-        save_to_json_file(list1, "add_item.json")
 
-    args = list(sys.argv)
-
-    for i in range(1, len(args)):
-        list1.append(args[i])
-
-    save_to_json_file(list1, "add_item.json")
