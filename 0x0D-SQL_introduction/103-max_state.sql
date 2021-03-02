029fbdc8a8cd04f681f44b1c98ef2aa1d63f38e0
sebastian <sebastian.calleu@gmail.com>
2021-03-01 23:39:59 -0500

    modifying the AS for temp

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- script to list max value of tmp of each state.
 
-SELECT state, max(value) AS max_tmp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, max(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
