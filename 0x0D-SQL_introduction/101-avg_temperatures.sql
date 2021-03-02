ded3ba2763cd091f72a8de7f248ef13dacdc6b3c
sebastian <sebastian.calleu@gmail.com>
2021-03-01 23:06:37 -0500

    adding AS to avg function.

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- script that list city with their average temperature.
 
-SELECT city, AVG(value) FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
