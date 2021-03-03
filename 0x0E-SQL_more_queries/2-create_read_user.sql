7bc42129b0d21f8677058e25deaf962d3aa8119c
sebastian <sebastian.calleu@gmail.com>
2021-03-02 21:20:47 -0500

    first time.

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- script to create a database and a user with select privilege.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
