57bb6d0c8bd7cfb71bc7b2651ee03b51279e706c
sebastian <sebastian.calleu@gmail.com>
2021-03-23 13:31:09 -0500

    fixing semistandard.

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,35 +1,35 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor (w, h) {
-	  if ((w > 0) && (h > 0)) {
-		this.width = w;
-		this.height = h;
-	  }
-	}
-  
-	print () {
-	  let count = 0;
-	  for (count; count < this.height; count++) {
-		console.log('X'.repeat(this.width));
-	  }
-	}
-  
-	rotate () {
-	  const tmp = this.width;
-	  this.width = this.height;
-	  this.height = tmp;
-	}
-  
-	double () {
-	  this.width *= 2;
-	  this.height *= 2;
-	}
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
-  class Square extends Rectangle {
-	  constructor(size) {
-		  super(size, size);
-	  }
+  print () {
+    let count = 0;
+    for (count; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
   }
-  module.exports = Square;
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
