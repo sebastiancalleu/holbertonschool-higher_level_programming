1156a8b355411d1391b50e3eeccd076641e0c7b8
sebastian <sebastian.calleu@gmail.com>
2021-03-23 20:06:41 -0500

    adding file for first time.

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const firstfile = process.argv[2];
+const secondfile = process.argv[3];
+const concatfile = process.argv[4];
+const fs = require('fs');
+const data = fs.readFileSync(firstfile, 'utf8');
+const data1 = fs.readFileSync(secondfile, 'utf8');
+const data2 = data + '\n' + data1 + '\n';
+
+fs.writeFileSync(concatfile, data2);
