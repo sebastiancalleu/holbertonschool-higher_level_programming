062b5ebc5266ea0b4efce87538b600b7fc685f00
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:52:46 -0500

    adding doc and fixing pep8

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+script to define a City class.
+'''
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,8 +10,13 @@ from model_state import Base, State
 
 
 class City(Base):
+    '''
+    City class
+    this class is a conection to the
+    cities table.
+    '''
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))
     states = relationship('State')
