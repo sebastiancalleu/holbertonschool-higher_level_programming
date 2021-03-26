83aece234f027ee650349494e84f8bb898dc84ed
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:49:53 -0500

    adding doc and fixing pep8

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+'''
+this script delete all the elements in states table
+that have an a inside his name.
+'''
 
 import sys
 from model_state import Base, State
@@ -8,11 +11,14 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).filter(State.name.like('%a%')):
+    for state in (session.query(State).order_by(State.id)
+                  .filter(State.name.like('%a%'))):
         session.delete(state)
     session.commit()
     session.close()
