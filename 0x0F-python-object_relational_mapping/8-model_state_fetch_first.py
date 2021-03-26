e911fc7fd1d83689166e9f523a710fd3bdae471a
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:22:30 -0500

    adding doc and fixing pep8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''
+Select the first row from the state table
+the print it.
+'''
 
 
 import sys
@@ -8,7 +12,9 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
