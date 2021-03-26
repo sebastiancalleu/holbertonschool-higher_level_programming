d514a5bc17532272e1db638900df55448b104da8
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:46:17 -0500

    adding doc and fixing pep8

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+'''
+This script adds a element to the states table
+'''
 
 import sys
 from model_state import Base, State
@@ -8,11 +10,13 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    new = State(name = 'Louisiana')
+    new = State(name='Louisiana')
     session.add(new)
     session.commit()
     session.close()
