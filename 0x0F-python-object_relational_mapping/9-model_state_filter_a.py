6566514bad49dcd83fd8f399d32c581e84520a23
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:28:27 -0500

    adding doc and fixing pep8

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''
+Select data from states table and filter it
+by the names that contain an a
+then print it.
+'''
 
 
 import sys
@@ -8,10 +13,13 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).filter(State.name.like('%a%')): # HERE: no SQL query, only objects!
+    for state in (session.query(State).order_by(State.id)
+                  .filter(State.name.like('%a%'))):
         print("{}: {}".format(state.id, state.name))
     session.close()
