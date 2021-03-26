06a68d7c2a55d7abdeafcff94c4b4b71d094cc6e
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:11:05 -0500

    adding doc and fixing pep8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-
+'''
+Script to create the State class
+'''
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    '''
+    State class
+    this class will represent
+    the table states
+    '''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
