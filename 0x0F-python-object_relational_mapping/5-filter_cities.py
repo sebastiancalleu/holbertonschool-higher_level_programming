49d5481b366276c6c6dbd3cd44cf3d4c8509fea0
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:08:35 -0500

    adding doc, fixing pep8 and adding edge case (no state).

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-
+'''
+Select cities names from states joined from cities
+and filter it for the state name that the user enter
+then print it in comma separated format.
+'''
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4], )) # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT cities.name FROM cities\
+        JOIN states ON cities.state_id = states.id\
+            WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4], ))
     query_rows = cur.fetchall()
-    for i in range(len(query_rows)):
-        if i + 1 == len(query_rows):
-            print(query_rows[i][0])
-        else:
-            print(query_rows[i][0] + ', ', end="")
+    if len(query_rows) == 0:
+        print()
+    else:
+        for i in range(len(query_rows)):
+            if i + 1 == len(query_rows):
+                print(query_rows[i][0])
+            else:
+                print(query_rows[i][0] + ', ', end="")
     cur.close()
     conn.close()
