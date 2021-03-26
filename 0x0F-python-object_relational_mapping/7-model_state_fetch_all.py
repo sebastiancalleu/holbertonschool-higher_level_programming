708e0e6bc8d6b87b468ad0d872760ff8ffd4122f
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:20:05 -0500

    adding doc and fixing pep8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+'''
+Selec all data from states and order it
+by state id, then print it
+'''
 
 import sys
 from model_state import Base, State
@@ -8,10 +11,12 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2],
+                            sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
