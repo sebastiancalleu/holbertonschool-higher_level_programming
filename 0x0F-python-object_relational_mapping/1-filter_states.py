4d3b65fa46c84201fb6403d32ed5446967e98154
sebastian <sebastian.calleu@gmail.com>
2021-03-26 09:48:32 -0500

    adding files for first time.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""
+Select all data from the states table
+which the name starts with an N and
+print it.
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states\
+        WHERE states.name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
