545a069bc5abbeae959e32b6e854c7778cd9d044
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:02:39 -0500

    adding doc and fixing pep8

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+'''
+Select some parameter from a table states
+joined from cities and then print it
+'''
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC") # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+        JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
