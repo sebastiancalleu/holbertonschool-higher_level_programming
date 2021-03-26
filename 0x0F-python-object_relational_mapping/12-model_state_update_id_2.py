c9673fc40f1f7014593551ce59393b598595d264
sebastian <sebastian.calleu@gmail.com>
2021-03-26 10:47:36 -0500

    adding doc and fixing pep8.

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+This scripts update a element from the states table.
+'''
 
 
 import sys
@@ -8,7 +11,9 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
