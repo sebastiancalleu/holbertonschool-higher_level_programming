443c4a1da0f0ca4093a53e8a5e74cbbb52f99c1b
sebastian <sebastian.calleu@gmail.com>
2021-03-28 15:37:06 -0500

    adding binary to query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states\
-        WHERE states.name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+        WHERE BINARY states.name = '{}' ORDER BY id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
