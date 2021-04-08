c0f0bfe50b82893dc82729795c5bd87dff5c3e10
sebastian <sebastian.calleu@gmail.com>
2021-04-08 17:15:41 -0500

    using sort

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,4 +8,5 @@ def find_peak(list_of_integers):
     """
     if len(list_of_integers) == 0:
         return None
-    return max(list_of_integers)
+    list_of_integers.sort(reverse=True)
+    return list_of_integers[0]
