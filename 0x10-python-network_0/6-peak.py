230424573a41366c6434923b263f7727667a9398
sebastian <sebastian.calleu@gmail.com>
2021-04-08 17:10:01 -0500

    adding docstring

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-# python script to find peak
+""" python script to find peak """
 
 
 def find_peak(list_of_integers):
+    """
+    python script to find peak
+    """
     if len(list_of_integers) == 0:
         return None
     return max(list_of_integers)
