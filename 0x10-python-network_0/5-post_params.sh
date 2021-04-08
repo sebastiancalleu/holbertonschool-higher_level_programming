4e7dd2a28f1aa613a0141a982312bd8cf518b260
sebastian <sebastian.calleu@gmail.com>
2021-04-08 14:56:45 -0500

    modifying the data entry

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to add variables.
-curl -s -X POST-d 'email: hr@holbertonschool.com&subject: I will always be here for PLD' "$1"
+curl -s -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"
