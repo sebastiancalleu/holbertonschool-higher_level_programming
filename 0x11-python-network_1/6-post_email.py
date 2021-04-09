282fa3dbffaf5e0581d18a83016780fa42518b21
sebastian <sebastian.calleu@gmail.com>
2021-04-09 16:36:58 -0500

    adding file for first time

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" script to request a URL """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    myobj = {"email": sys.argv[2]}
+    r = requests.post(sys.argv[1], data=myobj)
+    print(r.text)
