90469afc857feaef64d3a119fc03b499da1f904d
sebastian <sebastian.calleu@gmail.com>
2021-04-09 14:02:17 -0500

    adding name == main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,11 @@
 
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    the_page = response.read()
-print("Body response:")
-print("\t- type: ", type(the_page))
-print("\t- content: ", the_page)
-print("\t- utf8 content:", the_page.decode("UTF-8"))
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        the_page = response.read()
+    print("Body response:")
+    print("\t- type: ", type(the_page))
+    print("\t- content: ", the_page)
+    print("\t- utf8 content:", the_page.decode("UTF-8"))
