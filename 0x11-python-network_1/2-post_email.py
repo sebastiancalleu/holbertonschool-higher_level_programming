46247a3ba7c88a0aa99b8909a8a06bbe9a4e9afd
sebastian <sebastian.calleu@gmail.com>
2021-04-09 15:00:19 -0500

    adding file for first time.

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+""" script to request a URL """
+
+import urllib.request
+import urllib
+import sys
+
+if __name__ == "__main__":
+
+    email = urllib.parse.urlencode("email": sys.argv[2])
+    email = email.encode("ascii")
+    req = urllib.request.Request(sys.arv[1], email)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode("UTF-8"))
