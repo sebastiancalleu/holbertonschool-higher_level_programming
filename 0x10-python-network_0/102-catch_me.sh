bcb93b562cd39d6128612711df6b72b3b28d7ba9
sebastian <sebastian.calleu@gmail.com>
2021-04-08 19:19:39 -0500

    deleting arg

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to catch them
-curl -s -L -X PUT "$1" -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -s -L -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
