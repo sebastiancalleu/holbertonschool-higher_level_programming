27a2f3b714aa466a3851d1a4624773a89f06c187
sebastian <sebastian.calleu@gmail.com>
2021-04-09 14:04:16 -0500

    adding format string

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         the_page = response.read()
     print("Body response:")
-    print("\t- type: ", type(the_page))
-    print("\t- content: ", the_page)
-    print("\t- utf8 content:", the_page.decode("UTF-8"))
+    print("\t- type: {}".format(type(the_page)))
+    print("\t- content: {}".format(the_page))
+    print("\t- utf8 content: {}".format(the_page.decode("UTF-8")))
