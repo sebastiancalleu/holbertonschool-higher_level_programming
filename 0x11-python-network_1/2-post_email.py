508a23dac6e1f6eb87226f78566f43a28ded91a3
sebastian <sebastian.calleu@gmail.com>
2021-04-09 15:04:10 -0500

    fixing dict

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     email = urllib.parse.urlencode({"email": sys.argv[2]})
     email = email.encode("ascii")
-    req = urllib.request.Request(sys.arv[1], email)
+    req = urllib.request.Request(sys.argv[1], email)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
         print(the_page.decode("UTF-8"))
