1fbb2ef45a3029fa9215b4d80e7516025e00b219
sebastian <sebastian.calleu@gmail.com>
2021-04-10 12:40:58 -0500

    fixing pep8

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,14 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    """repo = sys.argv[1]
-    owner = sys.argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    session = requests.Session()
-    session = session.get(url)
-    data = session.json()
-    """
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1]))
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format
+                     (sys.argv[2], sys.argv[1]))
     data = r.json()
     if r.status_code == requests.codes.ok:
         for i, obj in enumerate(data):
@@ -20,4 +14,3 @@ if __name__ == "__main__":
                 break
             print("{}: {}".format(obj['sha'],
                                   obj['commit']['author']['name']))
-    #session.close()
