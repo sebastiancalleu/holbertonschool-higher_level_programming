ab0e41a344259c0e350d6e23a807cabef117e286
sebastian <sebastian.calleu@gmail.com>
2021-04-10 11:53:25 -0500

    adding conditional for status code 200

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,6 +7,7 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
-    for i in range(10):
-        print("{}: {}".format(r.json()[i]['sha'],
-                              r.json()[i]['commit']['author']['name']))
+    if r.status_code == 200:
+        for i in range(10):
+            print("{}: {}".format(r.json()[i]['sha'],
+                                  r.json()[i]['commit']['author']['name']))
