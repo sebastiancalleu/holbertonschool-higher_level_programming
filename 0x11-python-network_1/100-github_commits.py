cb58f4c9e1429b18d5cbdebbb8d9a6d9ee2d61a7
sebastian <sebastian.calleu@gmail.com>
2021-04-10 11:43:12 -0500

    modifying print format adding :

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,5 +8,5 @@ import sys
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
     for i in range(10):
-        print((r.json()[i]['sha']) + " " +
+        print((r.json()[i]['sha']) + ": " +
               (r.json()[i]['commit']['author']['name']))
