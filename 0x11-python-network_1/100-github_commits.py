ea05f66721ba5b6a23a46534a8e5d258187e64d1
sebastian <sebastian.calleu@gmail.com>
2021-04-10 12:04:23 -0500

    modifying the conditional

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     session = requests.Session()
     session = session.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
-    if session.status_code == 200:
+    if session.status_code == requests.codes.ok:
         for i in range(10):
             print("{}: {}".format(session.json()[i]['sha'],
                                   session.json()[i]['commit']['author']['name']))
