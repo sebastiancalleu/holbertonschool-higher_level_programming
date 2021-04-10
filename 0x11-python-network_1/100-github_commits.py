192878d930bc04eaf7e32658854bc8f501f543e9
sebastian <sebastian.calleu@gmail.com>
2021-04-10 12:01:22 -0500

    using session object

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,8 +6,9 @@ import sys
 
 
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
-    if r.status_code == 200:
+    session = requests.Session()
+    session = session.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
+    if session.status_code == 200:
         for i in range(10):
-            print("{}: {}".format(r.json()[i]['sha'],
-                                  r.json()[i]['commit']['author']['name']))
+            print("{}: {}".format(session.json()[i]['sha'],
+                                  session.json()[i]['commit']['author']['name']))
