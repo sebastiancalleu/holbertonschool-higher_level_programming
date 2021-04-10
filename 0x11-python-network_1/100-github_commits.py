b3316dab4bf4bef7c91a90af2a4403d484ad5a79
sebastian <sebastian.calleu@gmail.com>
2021-04-10 12:31:06 -0500

    adding session object

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,9 +8,10 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
     url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    r = requests.get(url)
-    data = r.json()
-    if r.status_code == requests.codes.ok:
+    session = requests.Session()
+    session = session.get(url)
+    data = session.json()
+    if session.status_code == requests.codes.ok:
         for i in range(10):
             print("{}: {}".format(data[i]['sha'],
                                   data[i]['commit']['author']['name']))
