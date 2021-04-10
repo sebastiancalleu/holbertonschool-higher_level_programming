f7278dcf52eb8fac66356146393f31bf587299db
sebastian <sebastian.calleu@gmail.com>
2021-04-10 12:08:49 -0500

    anaging a counter

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,10 +6,14 @@ import sys
 
 
 if __name__ == "__main__":
+    count = 0
     session = requests.Session()
     session = session.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
     if session.status_code == requests.codes.ok:
-        for i in range(10):
-            print("{}: {}".format(session.json()[i]['sha'],
-                                  session.json()[i]['commit']['author']['name']))
+        for i in session.json():
+            if count == 10:
+                break
+            print("{}: {}".format(i['sha'],
+                                  i['commit']['author']['name']))
+            count += 1
     session.close()
