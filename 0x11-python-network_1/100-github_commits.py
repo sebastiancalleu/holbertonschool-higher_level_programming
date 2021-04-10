c86015c507319e0186f7f7025ffaf4fe68d49ee7
sebastian <sebastian.calleu@gmail.com>
2021-04-10 12:39:43 -0500

    verifying the conditional

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,16 +5,19 @@ import sys
 
 
 if __name__ == "__main__":
-    repo = sys.argv[1]
+    """repo = sys.argv[1]
     owner = sys.argv[2]
     url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
     session = requests.Session()
     session = session.get(url)
     data = session.json()
-    if session.status_code == requests.codes.ok:
+    """
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1]))
+    data = r.json()
+    if r.status_code == requests.codes.ok:
         for i, obj in enumerate(data):
             if i == 10:
                 break
             print("{}: {}".format(obj['sha'],
                                   obj['commit']['author']['name']))
-    session.close()
+    #session.close()
