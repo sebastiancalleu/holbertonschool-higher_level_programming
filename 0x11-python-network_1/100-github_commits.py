47a7a87a2643549277e28524b018641a12b33d5a
sebastian <sebastian.calleu@gmail.com>
2021-04-10 12:37:09 -0500

    modifying conditional

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,19 +4,17 @@ import requests
 import sys
 
 
-def main(url):
-    session = requests.Session()
-    session = session.get(url)
-    data = session.json()
-    if session.status_code == requests.codes.ok:
-        for i in range(10):
-            print("{}: {}".format(data[i]['sha'],
-                                  data[i]['commit']['author']['name']))
-    session.close()
-
 if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
     url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-
-    main(url)
+    session = requests.Session()
+    session = session.get(url)
+    data = session.json()
+    if session.status_code == requests.codes.ok:
+        for i, obj in enumerate(data):
+            if i == 10:
+                break
+            print("{}: {}".format(obj['sha'],
+                                  obj['commit']['author']['name']))
+    session.close()
