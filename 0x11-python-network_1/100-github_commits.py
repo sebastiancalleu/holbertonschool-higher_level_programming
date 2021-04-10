25e7805f6de47daffa06fc594263ef58dd390e29
sebastian <sebastian.calleu@gmail.com>
2021-04-10 12:12:07 -0500

    storaging the json request in a variable

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-""" script to get the last 10 commits """
-
+"""script to get the last 10 commits"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    count = 0
     session = requests.Session()
     session = session.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
     if session.status_code == requests.codes.ok:
-        for i in session.json():
-            if count == 10:
-                break
-            print("{}: {}".format(i['sha'],
-                                  i['commit']['author']['name']))
-            count += 1
+        jsondata = session.json()
+        for i in range(10):
+            print("{}: {}".format(jsondata[i]['sha'],
+                                  jsondata[i]['commit']['author']['name']))
     session.close()
