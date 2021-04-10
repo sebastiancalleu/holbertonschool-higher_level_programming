75b74cc04ad878c10484fce9023f3d08389d4b9d
sebastian <sebastian.calleu@gmail.com>
2021-04-10 11:49:23 -0500

    adding format to string print

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script to get the las 10 commits """
+""" script to get the last 10 commits """
 
 import requests
 import sys
@@ -8,5 +8,5 @@ import sys
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
     for i in range(10):
-        print((r.json()[i]['sha']) + ": " +
-              (r.json()[i]['commit']['author']['name']))
+        print("{}: {}".format(r.json()[i]['sha'],
+                              r.json()[i]['commit']['author']['name']))
