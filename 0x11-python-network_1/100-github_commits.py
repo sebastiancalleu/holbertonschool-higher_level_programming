e1e2472c96cc2a9022716245eb0e96c89bc06711
sebastian <sebastian.calleu@gmail.com>
2021-04-10 11:42:09 -0500

    using arguments

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+""" script to get the las 10 commits """
 
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/repos/rails/rails/commits')
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]))
     for i in range(10):
         print((r.json()[i]['sha']) + " " +
               (r.json()[i]['commit']['author']['name']))
