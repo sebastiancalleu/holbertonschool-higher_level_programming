2184c3d2882b49db174f74f8fb8179c47aaf1997
sebastian <sebastian.calleu@gmail.com>
2021-05-04 20:10:32 -0500

    adding file for first time.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, res, body) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('code:', res.statusCode);
+});
