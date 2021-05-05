b4967008211c49ba097cdaee5b99433715f36529
sebastian <sebastian.calleu@gmail.com>
2021-05-04 21:35:32 -0500

    fixing the response code conditional

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,18 +6,19 @@ const ID = 18;
 const characterurl = 'https://swapi-api.hbtn.io/api/people/' + ID + '/';
 
 request(process.argv[2], function (err, res, body) {
-  if (err) {
+  if (res.statusCode === 200) {
+    const dct1 = JSON.parse(body);
+    const dct2 = dct1.results;
+    let count = 0;
+    let items = 0;
+    let character = 0;
+    for (items in dct2) {
+      for (character in dct2[items].characters) {
+        if (dct2[items].characters[character] === characterurl) { count += 1; }
+      }
+    }
+    console.log(count);
+  } else {
     return console.log(err);
   }
-  const dct1 = JSON.parse(body);
-  const dct2 = dct1.results;
-  let count = 0;
-  let items = 0;
-  let character = 0;
-  for (items in dct2) {
-    for (character in dct2[items].characters) {
-      if (dct2[items].characters[character] === characterurl) { count += 1; }
-    }
-  }
-  console.log(count);
 });
